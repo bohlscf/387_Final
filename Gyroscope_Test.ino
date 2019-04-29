231b08f30373e89bfd516724656c635bae9f1299
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-04-28 21:24:25 -0400

    Add files via upload

---
+++ Gyroscope_Test.ino
@@ -0,0 +1,54 @@
+//https://create.arduino.cc/projecthub/Nicholas_N/how-to-use-the-accelerometer-gyroscope-gy-521-6dfc19
+#include<Wire.h>
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+int green = 7;
+  int reps = 0;
+  int repFlag = 0;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+
+  pinMode(green, OUTPUT);
+}
+void loop(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  /*
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ); 
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.println(" ");
+*/
+  Serial.println(reps);
+  if(AcY > 25000 && repFlag == 0){
+    digitalWrite(green, HIGH); 
+    repFlag++;
+  }
+  else if (AcY < 15000 && repFlag == 1){
+    repFlag = 0;
+    digitalWrite(green, LOW);
+    reps++;
+  }
+  delay(100);
+}
---
+++ Main_Muscle_Code.ino
@@ -0,0 +1,29 @@
+int flag = 1;
+int rep = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int muscleVal = analogRead(A0);
+  Serial.println(muscleVal);
+  delay(200);
+
+  if(muscleVal > 500 && flag == 1){
+    Serial.println(muscleVal);
+    delay(50);
+    flag = 0;
+    rep++;
+    Serial.print("Rep is: ");
+    Serial.println(rep);
+  }
+
+  else if(muscleVal < 300 && flag == 0){
+    flag = 1;
+    delay(50);
+  }
+}
