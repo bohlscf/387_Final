f503b01f6fdab3e8abf2ef17c37170d4447f4d40
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-05-17 11:37:54 -0400

    Add files via upload

--- Main_Code.ino
+++ Main_Code.ino
@@ -3,13 +3,20 @@
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
-//SCL - A5; SDA - A4; green - 7; button - 2
+//SCL - A5; SDA - A4; green - 7; timerButton - 2; buzzer - 3; resetRepCount - 13
 int green = 7;
 int buttonPin = 2;
+int buzzerPin = 3;
+int resetPin = 13;
 
 int reps = 0;
+int lastReps = 0;
 int repFlag = 0;
+int resetFlag = 0;
 int timer = 0;  
+int timerReady = 0;
+int count = 0;
+
 
 void setup(){
   Wire.begin();
@@ -21,8 +28,14 @@ void setup(){
 
   pinMode(green, OUTPUT);
   pinMode(buttonPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
 }
+
+
 void loop(){
+
+//---------------  Rep Count  ----------------------------------------------------------------------------------------------------------------------------
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  
   Wire.endTransmission(false);
@@ -32,39 +45,94 @@ void loop(){
   AcZ=Wire.read()<<8|Wire.read();  
   GyX=Wire.read()<<8|Wire.read();  
   GyY=Wire.read()<<8|Wire.read();  
-  GyZ=Wire.read()<<8|Wire.read();  
-  /*
-  Serial.print("Accelerometer: ");
-  Serial.print("X = "); Serial.print(AcX);
-  Serial.print(" | Y = "); Serial.print(AcY);
-  Serial.print(" | Z = "); Serial.println(AcZ); 
-  
+  GyZ=Wire.read()<<8|Wire.read();
+
+    if(count == 10){
+    count = 0;
+//  Serial.print("Accelerometer: ");
+//  Serial.print("X = "); Serial.print(AcX);
+//  Serial.print(" | Y = "); Serial.println(AcY);
+//  Serial.print(" | Z = "); Serial.println(AcZ); 
+}
+/*
   Serial.print("Gyroscope: ");
   Serial.print("X = "); Serial.print(GyX);
   Serial.print(" | Y = "); Serial.print(GyY);
   Serial.print(" | Z = "); Serial.println(GyZ);
   Serial.println(" ");
 */
-  Serial.println(reps);
+
+  
   if(AcY > 25000 && repFlag == 0){
     digitalWrite(green, HIGH); 
     repFlag++;
+     
   }
   else if (AcY < 15000 && repFlag == 1){
     repFlag = 0;
     digitalWrite(green, LOW);
     reps++;
+    Serial.print("Rep count = ");
+    Serial.println(reps);
   }
   delay(100);
+  count++;
+
+  if(digitalRead(resetPin) == HIGH && repFlag == 0 && resetFlag == 0){
+    Serial.print("Past reps are: ");
+    Serial.println(lastReps);
+    Serial.print("Current reps are: ");
+    Serial.println(reps);
+    Serial.println("Rep count is reset");
+    lastReps = reps;
+    reps = 0;
+    resetFlag = 1;
+    delay(100);    
+  }
+  else if(digitalRead(resetPin) == LOW && resetFlag == 1){
+    resetFlag = 0;
+  }
+
+
+//----------------  Timer  -------------------------------------------------------------------------------------------------------------------------- 
+
+  if(digitalRead(buttonPin) == HIGH && timerReady == 0){
+      delay(35);
+      while(digitalRead(buttonPin) == HIGH);
+      timer = 5;
+      Serial.println("");
+      Serial.println("TIME HAS BEGUN");
+      Serial.println(timer);
+      timerReady = 1;
+  }
   
-  if(digitalRead(buttonPin) == HIGH && timer == 0){
-    Serial.println("I HAVE BEEN PRESSED");
-    timer = 30;
-    Serial.println(timer);
+  while( digitalRead(buttonPin) == LOW && timerReady == 1){
+      timer--;
+      int count = 0;
+      while(count < 100){// && digitalRead(buttonPin) == LOW){
+        delay(10); 
+        count++;
+        while(digitalRead(buttonPin) == HIGH){
+          delay(50);
+          count = 1000;
+          timerReady = 0;
+          timer = 0;
+        }
+      }
+      if(timer == 0 && count != 1000 ){
+        Serial.println("TIME IS UP!!");
+        timerReady = 0;
+        tone(buzzerPin, 400);
+        delay(2000);
+        noTone(buzzerPin);
+      }
+      else if(timer == 0){
+        Serial.println("TIMER IS STOPPED");
+        tone(buzzerPin, 400);
+        delay(500);
+        noTone(buzzerPin);
+      }
+      Serial.println(timer);
   }
-  while(timer > 0 && digitalRead(buttonPin) == LOW){
-    timer--;
-    delay(1000);    
-    Serial.println(timer);
-  }
-}
+  
+} 
